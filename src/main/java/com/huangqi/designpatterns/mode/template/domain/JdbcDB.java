53eb8cfba842ab571eacf0509bcdf2fff643901a
huangqi <huangqi@SSC-huangqi.com>
2023-05-25 21:49:28 +0800

    fix

--- src/main/java/com/huangqi/designpatterns/mode/template/domain/JdbcDB.java
+++ src/main/java/com/huangqi/designpatterns/mode/template/domain/JdbcDB.java
@@ -4,7 +4,7 @@ package com.huangqi.designpatterns.mode.template.domain;
  * @author huangqi
  * @date 2023-05-25 17:36
  */
-public class JdbcDB extends AbstractDB{
+public class JdbcDB extends AbstractDB {
 
 
     @Override
